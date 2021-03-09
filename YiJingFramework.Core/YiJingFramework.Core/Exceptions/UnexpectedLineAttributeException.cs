f1aa556f9b0bd57d9422b4f42cdcc2c74282fb3d
yueyinqiu <lujiaqi2003@126.com>
2021-03-09 22:15:34 +0800

    v2

--- YiJingFramework.Core/YiJingFramework.Core/Exceptions/UnexpectedLineAttributeException.cs
+++ YiJingFramework.Core/YiJingFramework.Core/Exceptions/UnexpectedLineAttributeException.cs
@@ -6,13 +6,28 @@ using System.Threading.Tasks;
 
 namespace YiJingFramework.Core.Exceptions
 {
+    /// <summary>
+    /// Exception that occurs when receiving a <see cref="LineAttribute"/> whose value is neither <see cref="LineAttribute.Yin"/> nor <see cref="LineAttribute.Yang"/>. 
+    /// </summary>
     [Serializable]
     public class UnexpectedLineAttributeException : Exception
     {
+        /// <summary>
+        /// The value received.
+        /// </summary>
         public LineAttribute ReceivedValue { get; }
+        /// <summary>
+        /// Initialize a <see cref="UnexpectedLineAttributeException"/>.
+        /// </summary>
+        /// <param name="receivedValue">The value received.</param>
         public UnexpectedLineAttributeException(LineAttribute receivedValue)
             : this(receivedValue, $"Unexpected Line Attribute: {receivedValue}.") { }
-
+        /// <summary>
+        /// Initialize a <see cref="UnexpectedLineAttributeException"/>.
+        /// </summary>
+        /// <param name="receivedValue">The value received.</param>
+        /// <param name="message">The message.</param>
+        /// <param name="inner">The inner exception.</param>
         public UnexpectedLineAttributeException(
             LineAttribute receivedValue,
             string? message = null,
@@ -22,6 +37,11 @@ namespace YiJingFramework.Core.Exceptions
             this.ReceivedValue = receivedValue;
         }
 
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="info"></param>
+        /// <param name="context"></param>
         protected UnexpectedLineAttributeException(
           System.Runtime.Serialization.SerializationInfo info,
           System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
--- YiJingFramework.Core/YiJingFramework.Core/Painting.cs
+++ YiJingFramework.Core/YiJingFramework.Core/Painting.cs
@@ -9,12 +9,25 @@ using YiJingFramework.Core.Exceptions;
 
 namespace YiJingFramework.Core
 {
+    /// <summary>
+    /// 卦画。
+    /// Represents a painting made up by the yin and yang lines.
+    /// When you use this as an <see cref="IEnumerable"/> or an <see cref="IEnumerable{T}"/> , you will get the lower lines first.
+    /// </summary>
     public class Painting : IReadOnlyList<LineAttribute>, IComparable<Painting>, IEquatable<Painting>
     {
         private readonly LineAttribute[] checkedLines;
+        /// <summary>
+        /// Initializes a new instance of <seealso cref="Painting"/>.
+        /// </summary>
+        /// <param name="lines">The lines, with the lower ones going first.</param>
         public Painting(params LineAttribute[] lines)
             : this((IEnumerable<LineAttribute>)lines) { }
 
+        /// <summary>
+        /// Initializes a new instance of <seealso cref="Painting"/>.
+        /// </summary>
+        /// <param name="lines">The lines, with the lower ones going first.</param>
         public Painting(IEnumerable<LineAttribute> lines)
         {
             if (lines is null)
@@ -30,17 +43,43 @@ namespace YiJingFramework.Core
         }
 
         #region Collecting
+        /// <summary>
+        /// Get a line in the painting.
+        /// </summary>
+        /// <param name="index">
+        /// The index of the line you want to get.
+        /// The higher the value is, the upper the line it represents.
+        /// The minimum value is 0.
+        /// </param>
+        /// <returns>The line.</returns>
+        /// <exception cref="IndexOutOfRangeException"> <paramref name="index"/> is out of range.</exception>
         public LineAttribute this[int index]
             => this.checkedLines[index];
+
+        /// <summary>
+        /// Get the count of the lines in this painting.
+        /// </summary>
         public int Count
             => this.checkedLines.Length;
+        /// <summary>
+        /// Returns an enumerator that iterates through the collection.
+        /// When you use this enumerator, you will get the lower lines first.
+        /// </summary>
+        /// <returns>An enumerator that can be used to iterate through the collection.</returns>
         public IEnumerator<LineAttribute> GetEnumerator()
             => ((IEnumerable<LineAttribute>)this.checkedLines).GetEnumerator();
+
         IEnumerator IEnumerable.GetEnumerator()
             => this.checkedLines.GetEnumerator();
         #endregion
 
         #region Comparing
+
+        /// <summary>
+        /// Compares the current instance with another object of the same type and returns an integer that indicates whether the current instance precedes, follows, or occurs in the same position in the sort order as the other object.
+        /// </summary>
+        /// <param name="other">An object to compare with this instance.</param>
+        /// <returns>A value that indicates the relative order of the objects being compared.</returns>
         public int CompareTo(Painting? other)
         {
             if (other is null)
@@ -67,6 +106,11 @@ namespace YiJingFramework.Core
 
             return 0;
         }
+        /// <summary>
+        /// Returns the hash code for this instance.
+        /// It is an <see cref="int"/> equivalent of the painting when there are less then 32 lines.
+        /// </summary>
+        /// <returns>A hash code for the current instance.</returns>
         public override int GetHashCode()
         {
             unchecked
@@ -80,12 +124,22 @@ namespace YiJingFramework.Core
                 return result;
             }
         }
+        /// <summary>
+        /// Returns a value indicating whether this instance is equal to a specified object.
+        /// </summary>
+        /// <param name="other">An object to compare with this instance, or <c>null</c>.</param>
+        /// <returns><c>true</c> if <paramref name="other"/> is equal to this instance; otherwise, <c>false</c>.</returns>
         public override bool Equals(object? other)
         {
             if (other is Painting painting)
                 return this.checkedLines.SequenceEqual(painting.checkedLines);
             return false;
         }
+        /// <summary>
+        /// Returns a value indicating whether this instance and a specified <see cref="Painting"/> object represent the same value.
+        /// </summary>
+        /// <param name="other">An object to compare to this instance, or <c>null</c>.</param>
+        /// <returns><c>true</c> if <paramref name="other"/> is equal to this instance; otherwise, <c>false</c>.</returns>
         public bool Equals(Painting? other)
         {
             if (other is null)
@@ -93,6 +147,12 @@ namespace YiJingFramework.Core
             return this.checkedLines.SequenceEqual(other.checkedLines);
         }
 
+        /// <summary>
+        /// Returns a value indicating whether two <see cref="Painting"/> objects represent the same value.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns><c>true</c> if <paramref name="left"/> is equal to <paramref name="right"/>; otherwise, <c>false</c>.</returns>
         public static bool operator ==(Painting? left, Painting? right)
         {
             if (left is null)
@@ -102,6 +162,12 @@ namespace YiJingFramework.Core
             return left.checkedLines.SequenceEqual(right.checkedLines);
         }
 
+        /// <summary>
+        /// Returns a value indicating whether two <see cref="Painting"/> objects represent two different values.
+        /// </summary>
+        /// <param name="left"></param>
+        /// <param name="right"></param>
+        /// <returns><c>true</c> if <paramref name="left"/> isn't equal to <paramref name="right"/>; otherwise, <c>false</c>.</returns>
         public static bool operator !=(Painting? left, Painting? right)
         {
             if (left is null)
@@ -113,40 +179,66 @@ namespace YiJingFramework.Core
         #endregion
 
         #region Converting
+
+        /// <summary>
+        /// Returns a string that represents the painting.
+        /// You can use <seealso cref="TryParse(string?, out Painting)"/> to convert it back.
+        /// </summary>
+        /// <returns>The string.</returns>
         public override string ToString()
         {
-            StringBuilder stringBuilder = new StringBuilder(this.checkedLines.Length);
+            StringBuilder stringBuilder = new(this.checkedLines.Length);
             foreach (var line in this.checkedLines)
                 stringBuilder.Append((int)line);
             return stringBuilder.ToString();
         }
 
+        /// <summary>
+        /// Convert from a <see cref="string"/>.
+        /// </summary>
+        /// <param name="s">
+        /// The string represents the painting.
+        /// </param>
+        /// <param name="result">The painting.</param>
+        /// <returns>A value indicates whether it has been successfully converted or not.</returns>
         public static bool TryParse(
-            string s, 
-            [NotNullWhen(true)] out Painting? result,
-            bool useStrict = true)
+            [NotNullWhen(true)] string? s,
+            [MaybeNullWhen(false)] out Painting result)
         {
             if (s is null)
-                throw new ArgumentNullException(nameof(s));
-            var max = useStrict ? '1' : '9';
-            List<LineAttribute> r = new List<LineAttribute>(s.Length);
+            {
+                result = null;
+                return false;
+            }
+            List<LineAttribute> r = new(s.Length);
             foreach (var c in s)
             {
-                if (c < '0' || c > max)
+                switch (c)
                 {
-                    result = null;
-                    return false;
+                    case '0':
+                        r.Add(LineAttribute.Yin);
+                        break;
+                    case '1':
+                        r.Add(LineAttribute.Yang);
+                        break;
+                    default:
+                        result = null;
+                        return false;
                 }
-                r.Add((LineAttribute)(c % 2));
             }
             result = new Painting(r);
             return true;
         }
 
+        /// <summary>
+        /// Convert to a <see cref="byte"/> array.
+        /// You can convert it back by using <seealso cref="FromBytes(byte[])"/> .
+        /// </summary>
+        /// <returns>The byte that can represent this painting.</returns>
         public byte[] ToBytes()
         {
             var thisLength = this.checkedLines.Length;
-            BitArray bitArray = new BitArray(thisLength + 1);
+            BitArray bitArray = new(thisLength + 1);
             for (int i = 0; i < thisLength; i++)
                 bitArray.Set(i, Convert.ToBoolean((int)this.checkedLines[i]));
             bitArray.Set(thisLength, true);
@@ -155,12 +247,17 @@ namespace YiJingFramework.Core
             return bytes;
         }
 
+        /// <summary>
+        /// Convert from a <see cref="byte"/> array.
+        /// </summary>
+        /// <param name="bytes">The bytes that represents this painting.</param>
+        /// <returns>The painting.</returns>
         public static Painting FromBytes(params byte[] bytes)
         {
             if (bytes is null)
                 throw new ArgumentNullException(nameof(bytes));
-            BitArray bitArray = new BitArray(bytes);
-            List<LineAttribute> r = new List<LineAttribute>(bytes.Length);
+            BitArray bitArray = new(bytes);
+            List<LineAttribute> r = new(bytes.Length);
             bool notStarted = true;
             for (int i = bitArray.Length - 1; i >= 0; i--)
             {
